18d3edf6685c0c218be44965d5eca5cf8a44a212
Adam Pruner <adampruner99@gmail.com>
2019-08-21 12:49:07 -0700

    fix size of debug rects for tile map

--- ActionRPG/Tile.cpp
+++ ActionRPG/Tile.cpp
@@ -19,8 +19,8 @@ void Tile::initDebugRect()
 {
 	m_debugRect = sf::RectangleShape(sf::Vector2f(m_bounds.width, m_bounds.height));
 	m_debugRect.setPosition(sf::Vector2f(
-		(float)(m_xPosition * c_tileSideLengthPixels * c_roomScalingFactor / 2),
-		(float)(m_yPosition * c_tileSideLengthPixels * c_roomScalingFactor / 2)
+		(float)(m_xPosition * c_tileSideLengthPixels * c_roomScalingFactor),
+		(float)(m_yPosition * c_tileSideLengthPixels * c_roomScalingFactor)
 	));
 	m_debugRect.setFillColor(sf::Color::Transparent);
 	m_debugRect.setOutlineColor(sf::Color::Blue);
