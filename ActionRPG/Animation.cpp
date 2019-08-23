ef2e4b4b3ec6c2e15715be95d66b67c2a2dfda4c
Adam Pruner <adampruner99@gmail.com>
2019-08-23 12:25:00 -0700

    first attempt at implementing looping animation, test to come

--- ActionRPG/Animation.cpp
+++ ActionRPG/Animation.cpp
@@ -4,36 +4,66 @@ Animation::Animation(TextureMap *textureMap, std::string const& animName)
 {
 	m_textureMap = textureMap;
 	m_animName = animName;
-	m_animFrames = new std::vector<sf::Texture>();
 	initAnimFrames();
 	// Create the sprite from the first frame of the animation
-	m_sprite = sf::Sprite(m_animFrames->at(0));
+	m_sprite = sf::Sprite(m_textureMap->getSpriteSheet(), m_animSpriteSheetBounds);
+	m_isAnimating = false;
 }
 
 void Animation::initAnimFrames()
 {
 	sf::Texture spriteSheet = m_textureMap->getSpriteSheet();
 	std::tuple<int, int, int, int, int> animTuple = m_textureMap->getSpriteSheetAnimVecTuple(m_animName);
-	int topLeftX, topLeftY, width, height, frames;
-	topLeftX = std::get<0>(animTuple);
-	topLeftY = std::get<1>(animTuple);
-	width = std::get<2>(animTuple);
-	height = std::get<3>(animTuple);
-	frames = std::get<4>(animTuple);
-	for (int i = 0; i < frames; i++)
+	m_firstFrameTopLeft = sf::Vector2f((float)std::get<0>(animTuple), (float)std::get<1>(animTuple));
+	m_width = std::get<2>(animTuple);
+	m_height = std::get<3>(animTuple);
+	m_frames = std::get<4>(animTuple);
+	m_animSpriteSheetBounds = sf::IntRect((int)m_firstFrameTopLeft.x, (int)m_firstFrameTopLeft.y, m_width, m_height);
+	m_animIndex = 0;
+
+}
+
+void Animation::animate()
+{
+	if (!m_isAnimating)
 	{
-		sf::IntRect spriteSheetBounds(topLeftX + i * width, topLeftY, width, height);
-		sf::Sprite tmpSprite(spriteSheet, spriteSheetBounds);
-		m_animFrames->push_back(*tmpSprite.getTexture());
+		return;
+	}
+
+	float dtSeconds = m_clock->getElapsedTime().asSeconds();
+	if (dtSeconds > 1.0f)
+	{
+		updateAnimationFrame();
+	}
+}
+
+void Animation::updateAnimationFrame()
+{
+	if (m_animIndex < m_frames)
+	{
+		m_animSpriteSheetBounds.left += m_width;
+		m_sprite.setTextureRect(m_animSpriteSheetBounds);
+		m_animIndex++;
+	}
+	else
+	{
+		m_animSpriteSheetBounds.left = (int)m_firstFrameTopLeft.x;
+		m_sprite.setTextureRect(m_animSpriteSheetBounds);
+		m_animIndex = 0;
 	}
 }
 
 void Animation::startAnimation()
 {
-
+	m_isAnimating = true;
 }
 
 void Animation::stopAnimation()
 {
+	m_isAnimating = false;
+}
 
+bool Animation::isAnimating()
+{
+	return m_isAnimating;
 }
--- ActionRPG/Animation.h
+++ ActionRPG/Animation.h
@@ -5,14 +5,32 @@
 class Animation
 {
 private:
-	// Vector of animation frames
-	std::vector<sf::Texture> *m_animFrames;
+	// Represents the current frame of the animation in the sprite sheet
+	sf::IntRect m_animSpriteSheetBounds;
+
+	// Number of frames in the animation
+	int m_frames;
+
+	// Width of each frame
+	int m_width;
+
+	// Height of each frame
+	int m_height;
+
+	// Vector representing the topLeft point of the first frame
+	sf::Vector2f m_firstFrameTopLeft;
 
 	// Name of the animation
 	std::string m_animName;
 
-	// Sprite to be drawn
+	// Sprite to be drawn on the current frame
 	sf::Sprite m_sprite;
+
+	// Bool marking whether animation is happening
+	bool m_isAnimating;
+
+	// Current frame index
+	int m_animIndex;
 
 	// Clock instance
 	sf::Clock *m_clock;
@@ -20,9 +38,18 @@ private:
 	// TextureMap instance
 	TextureMap *m_textureMap;
 
+	// Adds the animation frames to m_animFrames vector
 	void initAnimFrames();
+
+	// Run the animation
+	void animate();
+
+	// Update the frame
+	void updateAnimationFrame();
 public:
 	Animation(TextureMap *textureMap, std::string const& animName);
+
+	bool isAnimating();
 
 	void startAnimation();
 	void stopAnimation();
