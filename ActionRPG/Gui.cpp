329b0eafcb48e6522507f536a6d2b39efd1bcea1
Adam Pruner <adampruner99@gmail.com>
2019-12-28 12:16:01 -0800

    decide against using Tgui, will create a gui myself from scratch with SFML and assets

--- ActionRPG/Gui.cpp
+++ ActionRPG/Gui.cpp
@@ -3,15 +3,12 @@
 Gui::Gui(sf::RenderWindow gameWindow)
 {
 	m_gameWindow = &gameWindow;
-	m_gui = new tgui::Gui(*m_gameWindow);
 }
 
 Gui::~Gui()
 {
-	delete m_gui;
 }
 
 void Gui::draw()
 {
-	m_gui->draw();
 }
--- ActionRPG/Gui.h
+++ ActionRPG/Gui.h
@@ -1,15 +1,12 @@
 #pragma once
 #include <SFML/Graphics.hpp>
-#include <TGUI/TGUI.hpp>
 
-// Abstract GUI class that can be extended to create other GUIs with TGUI
+// Abstract GUI class that can be extended to create other GUIs
 class Gui {
 private:
 	// Instance of the game window
 	sf::RenderWindow *m_gameWindow;
 
-	// Tgui gui instance
-	tgui::Gui *m_gui;
 
 public:
 	Gui(sf::RenderWindow gameWindow);
