d0d4963799f75a19d09a2e1173eb2918666490c8
Adam Pruner <adampruner99@gmail.com>
2020-01-15 22:30:50 -0800

    build out TilePropertiesMap

--- ActionRPG/TileConstants.h
+++ ActionRPG/TileConstants.h
@@ -3,16 +3,31 @@
 #include <string>
 
 class TileConstants {
+public:
 	// TileConstants includes constant maps for all of the tile data
 
 	// Map tileTextureName -> isSolid property
-	const static std::map<std::string, TileProperties> c_tileQualityMap;
-
+	const static std::map<std::string, TileProperties> c_tilePropertyMap;
 
 };
 
 // TileProperties is essentially a struct (class) that holds constant properties for each tile type
+// TileProperties will be populated in the above c_tilePropertyMap for each tile texture
 class TileProperties {
+private:
+	std::string m_tileTextureName;
+	bool m_isSolid;
+	bool m_isAnimated;
+	// TODO: Add more appropriate properties here if necessary
+
 public:
-	bool isSolid;
+	TileProperties(bool isSolid, bool isAnimated, std::string tileTextureName);
+	~TileProperties();
+
+	// Getters and Setters
+	// Getters
+	bool getIsSolid();
+	bool getIsAnimated();
+	std::string getTileTextureName();
+
 };
