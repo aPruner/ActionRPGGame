01c39f0c05e663b2636ceab867ace8331aa8be32
Adam Pruner <adampruner99@gmail.com>
2020-02-13 19:43:52 -0800

    frame out inventory methods

--- ActionRPG/Inventory.h
+++ ActionRPG/Inventory.h
@@ -6,13 +6,29 @@ private:
 
 	// Max number of items in the inventory
 	int m_maxSize;
+
+	// Number of gold the player currently has
+	int m_gold;
+
+	// Flag for inventory full
+	bool m_isFull;
 	
 	// Vector representation of the inventory
 	std::vector<Item> *m_items;
 
-
 public:
 	Inventory();
 	~Inventory();
+
+	// Getters and Setters
+	// Getters
+	int getMaxSize();
+	int getGold();
+	bool getIsFull();
+	
+	// Setters
+	void setMaxSize(int maxSize);
+	void setGold(int gold);
+	void setIsFull(bool isFull);
 
 };
