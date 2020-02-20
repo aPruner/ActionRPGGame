94d4b76dae16d54f55e507ea5d1a19c44a2eef05
Adam Pruner <adampruner99@gmail.com>
2020-02-19 16:16:57 -0800

    add InventoryConstants, start building out Inventory implementation

--- ActionRPG/Inventory.cpp
+++ ActionRPG/Inventory.cpp
@@ -1,2 +1,45 @@
 #include "Inventory.h"
 
+Inventory::Inventory()
+{
+	m_maxSize = InventoryConstants::c_defaultMaxSize;
+	m_gold = InventoryConstants::c_defaultStartingGold;
+	m_isFull = false;
+}
+
+Inventory::~Inventory()
+{
+}
+
+// Getters and Setters
+// Getters
+int Inventory::getGold()
+{
+	return m_gold;
+}
+
+int Inventory::getMaxSize()
+{
+	return m_maxSize;
+}
+
+bool Inventory::getIsFull()
+{
+	return m_isFull;
+}
+
+// Setters
+void Inventory::setGold(int gold)
+{
+	m_gold = gold;
+}
+
+void Inventory::setMaxSize(int maxSize)
+{
+	m_maxSize = maxSize;
+}
+
+void Inventory::setIsFull(bool isFull)
+{
+	m_isFull = isFull;
+}
---
+++ ActionRPG/InventoryConstants.h
@@ -0,0 +1,11 @@
+#pragma once
+
+class InventoryConstants {
+public:
+	// TODO: Change these values as necessary
+	const static int c_defaultMaxSize = 30;
+	const static int c_defaultStartingGold = 500;
+	
+	// TODO: Add more values as necessary
+
+};
